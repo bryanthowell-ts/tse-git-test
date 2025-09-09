58f0a8889caa6040e597cde3f0d9653488ec362a
Bryant Howell - ThoughtSpot <83678239+bryanthowell-ts@users.noreply.github.com>
2025-09-09 16:01:46 -0500

    Update test_script.py

--- .github/workflows/test_script.py
+++ .github/workflows/test_script.py
@@ -1,3 +1,31 @@
 import os
+import requests.exceptions
+import csv
+import json
 
-print(os.environ.get('SECRET_VAL'))
+from thoughtspot_rest_api_v1 import *
+
+# Example functions for reporting of various counts of objects
+# Useful for reporting various users / groups / domains / orgs for contractual compliance
+
+#
+server = os.environ.get('TS_SERVER')       # or type in yourself
+
+# Supply access token from REST API Playground or provide username/password securely
+full_access_token = os.environ.get('TS_TOKEN')
+
+
+ts: TSRestApiV2 = TSRestApiV2(server_url=server)
+if full_access_token != "":
+    ts.bearer_token = full_access_token
+
+# Get all Tables
+search_request = {
+    'metadata': {'type': 'LOGICAL_TABLE'},
+    'record_offset': 0,
+    'record_size': 10  # default is 10
+}
+tables = ts.metadata_search(request=search_request)
+
+print(json.dumps(tables, indent=2))
+
